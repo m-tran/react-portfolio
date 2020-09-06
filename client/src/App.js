779de028e3ea6945efd2fb37eb3ac69626af4d34
Michelle Tran <mptran0101@berkeley.edu>
2020-09-05 18:21:48 -0700

    created main about me section

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div>
-
+        <Main />
       </div>
     </div>
   );
---
+++ client/src/components/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./main.css";
+
+const Main = () => {
+    return (
+        <div className="main">
+            <h1 className="main-text">
+            <div className="name">Hey, I'm Michelle.</div>
+            <div className="about">I'm a creative designer (who codes) @&nbsp;
+            <a className="two-link" href="www.twofurnish.com">two</a>
+            <br/>
+            & I make brands come to&nbsp;
+            <a className="project" href="/projects">life</a>
+            &nbsp;through user and purpose-centered positioning.</div>
+            </h1>
+        </div>
+    )
+}
+
+export default Main;

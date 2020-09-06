c3600f9580829a5a853a4963bf81d7b2242cfb59
Michelle Tran <mptran0101@berkeley.edu>
2020-09-06 01:08:43 -0700

    text highlight animation

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -6,27 +6,39 @@ const Main = () => {
 
     return (
         <div className="main">
-            <h1 className="main-text">
-                <div className="name">Hey, I'm Michelle.</div>
-                <div className="about">I'm a creative designer (who codes) @&nbsp;
-                <a 
-                    className="two-link" 
-                    href="www.twofurnish.com"
-                    onMouseEnter={() => setIsShown(true)}
-                    onMouseLeave={() => setIsShown(false)}>
-                        two
-                </a>
-                <br/>
-                & I make brands come to&nbsp;
-                <a 
-                    className="project"
-                    href="/projects"
-                    onMouseEnter={() => setIsShown(true)}
-                    onMouseLeave={() => setIsShown(false)}>
-                        life
-                </a>
-                &nbsp;through user and purpose-centered positioning.</div>
-            </h1>
+            <div className="main-text">
+                <div className="name">
+                    <h1 className="name-text">Hey, I'm Michelle.</h1>
+                    <div className="name-color"></div>
+                    <div className="name-color-2"></div>
+                </div>
+                <div className="about">
+                    <h1 className="about-text">I'm a creative designer (who codes) @&nbsp;
+                        <a 
+                            className="two-link" 
+                            href="www.twofurnish.com"
+                            onMouseEnter={() => setIsShown(true)}
+                            onMouseLeave={() => setIsShown(false)}>
+                                two
+                        </a>
+                        <br/>
+                        & I make brands come to&nbsp;
+                        <a 
+                            className="project"
+                            href="/projects"
+                            onMouseEnter={() => setIsShown(true)}
+                            onMouseLeave={() => setIsShown(false)}>
+                                life
+                        </a>
+                        &nbsp;through user and purpose-centered positioning.
+                    </h1>
+                </div>
+                <div className="scroll-down">
+                    <h2>
+                        &#x21E9;
+                    </h2>
+                </div>
+            </div>
             {isShown && (
             <div className="blob"></div>
             )}

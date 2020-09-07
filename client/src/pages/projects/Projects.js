4bd0012a2f7fb54e34be015edd1512fd5161c0f4
Michelle Tran <mptran0101@berkeley.edu>
2020-09-07 14:36:14 -0700

    added link

--- client/src/pages/projects/Projects.js
+++ client/src/pages/projects/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, BrowserRouter as Router } from 'react-router-dom';
 import "./projects.css";
 import { Grid } from "@material-ui/core";
 import twoThumb from "../../components/img/Two.jpg";
@@ -9,33 +10,35 @@ import pairThumbGif from "../../components/img/divide_event_03.gif";
 const Projects = () => {
 
     return (
-        <Grid container spacing={2} align-items="center" justify="center" id="projects">
-            <Grid item xs={10} className="project-page">
-                <div className="title">
-                    <h1>Projects</h1>
-                </div>
+        <Router>
+            <Grid container spacing={2} align-items="center" justify="center" id="projects">
+                <Grid item xs={10} className="project-page">
+                    <div className="title">
+                        <h1>Projects</h1>
+                    </div>
+                </Grid>
+                <Grid item xs={5} p={10}>
+                <Link to={ pathname: "http://www.twofurnish.com" } target="_blank"><img 
+                        className="thumb" 
+                        src={twoThumb} 
+                        alt="two" 
+                        onMouseEnter={e => (e.currentTarget.src=twoThumbGif)}
+                        onMouseLeave={e => (e.currentTarget.src=twoThumb)}/></Link>
+                    <a href="www.twofurnish.com"><h2>two</h2></a>
+                    <p>Marketing, Graphic + Web Design</p>
+                </Grid>
+                <Grid item xs={5} className="pair">
+                    <a target="_blank" href="www.madebypair.com"><img 
+                        className="thumb" 
+                        src={pairThumb} 
+                        alt="pair" 
+                        onMouseEnter={e => (e.currentTarget.src=pairThumbGif)}
+                        onMouseLeave={e => (e.currentTarget.src=pairThumb)}/></a>
+                    <a href="www.madebypair.com"><h2>Pair</h2></a>
+                    <p>Brand + Web Design</p>
+                </Grid>
             </Grid>
-            <Grid item xs={5} p={10}>
-                <a target="_blank" href="www.twofurnish.com"><img 
-                    className="thumb" 
-                    src={twoThumb} 
-                    alt="two" 
-                    onMouseEnter={e => (e.currentTarget.src=twoThumbGif)}
-                    onMouseLeave={e => (e.currentTarget.src=twoThumb)}/></a>
-                <a href="www.twofurnish.com"><h2>two</h2></a>
-                <p>Marketing, Graphic + Web Design</p>
-            </Grid>
-            <Grid item xs={5} className="pair">
-                <a target="_blank" href="www.madebypair.com"><img 
-                    className="thumb" 
-                    src={pairThumb} 
-                    alt="pair" 
-                    onMouseEnter={e => (e.currentTarget.src=pairThumbGif)}
-                    onMouseLeave={e => (e.currentTarget.src=pairThumb)}/></a>
-                <a href="www.madebypair.com"><h2>Pair</h2></a>
-                <p>Brand + Web Design</p>
-            </Grid>
-        </Grid>
+        </Router>
     )
 }
 
